5c5ea5d304a6abc014964aa258e843898f54edf2
Murilo Guedes <muriloguedesmenezes2021@gmail.com>
2025-11-14 18:04:20 -0300

    first commit

---
+++ src/javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  })
+
+  ScrollReveal().reveal("#img-intro", {
+    origin: "right",
+    duration: 2000,
+    distance: "20%",
+  })
+})
